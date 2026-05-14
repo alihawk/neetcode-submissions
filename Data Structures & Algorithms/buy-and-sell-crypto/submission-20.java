756124189e0a247357af9c8505af66d6145c9afa
Ali Mahmood <67463234+alihawk@users.noreply.github.com>
2026-05-15 00:57:31 +0200

    Add: buy-and-sell-crypto - submission-20

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-20.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int minPrice = prices[0];
+        int profit =0;
+
+       for(int i =0; i < prices.length ; i++){
+            profit = prices[i]-minPrice;
+            maxProfit = Math.max(maxProfit,profit);
+            minPrice = Math.min(minPrice,prices[i]);
+       }
+       return maxProfit;
+    }
+}
